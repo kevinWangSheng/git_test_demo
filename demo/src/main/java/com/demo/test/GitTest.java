8ae2effd0fe60a9c2cf33329afaea36f818604fc
Kevin Wang <ltwoggbuty2mh@gmail.com>
2023-04-02 19:19:55 +0800

    hot-fix Commit

--- demo/src/main/java/com/demo/test/GitTest.java
+++ demo/src/main/java/com/demo/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is my first git project!!!!");
         System.out.println("secnod!!!!!!");
+        System.out.println("this is the hot-fix...............");
     }
 }
