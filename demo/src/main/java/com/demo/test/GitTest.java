a5ffde5cff4cf5f04de9b4afc24b6ceadb49ce31
Kevin Wang <ltwoggbuty2mh@gmail.com>
2023-04-02 19:07:12 +0800

    secondCommit

--- demo/src/main/java/com/demo/test/GitTest.java
+++ demo/src/main/java/com/demo/test/GitTest.java
@@ -7,5 +7,6 @@ package com.demo.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is my first git project!!!!");
+        System.out.println("secnod!!!!!!");
     }
 }
