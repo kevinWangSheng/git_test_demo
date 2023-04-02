a15df0e8eef87bc12a0d7cfa59c1ef217fd9d93e
Kevin Wang <ltwoggbuty2mh@gmail.com>
2023-04-02 19:01:25 +0800

    firstCommit

---
+++ demo/src/main/java/com/demo/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.demo.test;
+
+/**
+ * @author wang
+ * @create 2023-2023-02-18:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("this is my first git project!!!!");
+    }
+}
